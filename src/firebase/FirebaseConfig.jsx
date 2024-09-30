d8f008ad141a432896064d76603deafbcc7d4574
NishanttBelhekar <23nishantbelhekar@gmail.com>
2024-09-30 07:52:26 +0530

    Firebase Commit

--- src/firebase/FirebaseConfig.jsx
+++ src/firebase/FirebaseConfig.jsx
@@ -1,15 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBiT3G0Ysvxrz-3K1sxUfBwXEZVLFOy3Ls",
-  authDomain: "intershipbinary.firebaseapp.com",
-  projectId: "intershipbinary",
-  storageBucket: "intershipbinary.appspot.com",
-  messagingSenderId: "684390458887",
-  appId: "1:684390458887:web:156403e16f28e1d3ea6ca2",
-  measurementId: "G-346RDQW816",
-};
+
+
+ const firebaseConfig = {
+   apiKey: "",
+   authDomain: "",
+   projectId: "",
+   storageBucket: "",
+   messagingSenderId: "",
+   appId: "",
+   measurementId: "",
+ };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const database = getAuth(app);
